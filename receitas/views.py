710e77a08d3d5a152f5a4f6f1e69d5f336331843
guilhermepaiva <guilhermpaiva9@gmail.com>
2020-07-16 14:08:25 -0300

    structured index

--- receitas/views.py
+++ receitas/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Receitas</h1>')
+    return render(request, 'index.html')

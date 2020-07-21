2cd079df4e067439e47801f8318f75956931616e
guilhermepaiva <guilhermpaiva9@gmail.com>
2020-07-21 16:35:53 -0300

    exibindo dados do banco

--- receitas/views.py
+++ receitas/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Receita
+
 
 def index(request):
-    receitas = {
-        1:'Lasanha',
-        2:'Sopa de Legumes',
-        3:'Sorvete',
-        4:'Bolo de Chocolate'
-    }
+    receitas = Receita.objects.all()
     dados = {
-        'nome_das_receitas':receitas
+        'receitas':receitas
     }
     return render(request, 'index.html', dados)
 
